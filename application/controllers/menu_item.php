cb5fc1e87cbf5bf88205e2eb5b5c68187ad66654
James Johnson <James.Johnson@excellentingenuity.com>
2012-07-03 17:47:38 -0400

    initial commit 2
    Signed-off-by: James Johnson <James.Johnson@excellentingenuity.com>

---
+++ application/controllers/menu_item.php
@@ -0,0 +1,11 @@
+<?php
+class Menu_Item extends Controller {
+    function index() {
+        
+        $this->load->view('menu_item_list');
+    }
+ 
+ 
+    
+}
+?>
---
+++ application/controllers/ogg.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ */
+class Ogg extends Controller {
+	
+	function index() {
+		echo "my name is ogg";
+	}
+}
+
+?>
